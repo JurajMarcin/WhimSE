7b92ca03acc8e0ca0115c9d3293faac00419f2bb
Juraj Marcin <juraj@jurajmarcin.com>
2025-01-04 17:23:28 +0100

    Add wip main function
    Signed-off-by: Juraj Marcin <juraj@jurajmarcin.com>

---
+++ whimse/__init__.py
@@ -0,0 +1,53 @@
+import logging
+from pathlib import Path
+from sys import stderr
+from tempfile import mkdtemp
+
+from whimse.detect import PolicyChangesDetector
+from whimse.explore import explore_stage
+from whimse.explore.types import ExploreStageConfig, ModuleFetchMethod
+from whimse.report.types import Report
+from whimse.utils.logging import get_logger
+
+logging.basicConfig(level=logging.DEBUG, stream=stderr)
+
+
+_logger = get_logger(__name__)
+
+
+def main() -> None:
+    _logger.debug("started")
+
+    tmpdir = mkdtemp()
+    print(tmpdir)
+    explore_stage_result = explore_stage(
+        ExploreStageConfig(
+            Path("/var/lib/selinux/targeted"),
+            Path(tmpdir),
+            [
+                ModuleFetchMethod.LOCAL_MODULE,
+                ModuleFetchMethod.EXACT_PACKAGE,
+                ModuleFetchMethod.NEWER_PACKAGE,
+            ],
+        )
+    )
+
+    with open("dist_policy.txt", "w") as f:
+        for module, source in explore_stage_result.dist_policy.modules.items():
+            print(module, source, file=f)
+        print(explore_stage_result.dist_policy.local_modifications, file=f)
+        print(f"{explore_stage_result.dist_policy.dontaudit_disabled=}", file=f)
+    with open("actual_policy.txt", "w") as f:
+        for module in explore_stage_result.actual_policy.modules:
+            print(module, file=f)
+        print(explore_stage_result.actual_policy.local_modifications, file=f)
+        print(f"{explore_stage_result.actual_policy.dontaudit_disabled=}", file=f)
+
+    report = Report()
+    report.add_items(
+        PolicyChangesDetector(
+            explore_stage_result.actual_policy, explore_stage_result.dist_policy
+        ).detect_changes()
+    )
+
+    print(report)
