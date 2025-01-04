a3dbcd5f9d0960451f48acd735c590fc179c15aa
Juraj Marcin <juraj@jurajmarcin.com>
2025-01-04 17:07:58 +0100

    Add function to find semodule installs in shell script
    Signed-off-by: Juraj Marcin <juraj@jurajmarcin.com>

---
+++ whimse/utils/semodule.py
@@ -0,0 +1,25 @@
+import re
+from argparse import ArgumentParser
+from collections.abc import Iterable
+
+from whimse.utils.logging import get_logger
+from whimse.utils.shell import get_command_executions
+
+_logger = get_logger(__name__)
+
+_SEMODULE_ARG_PARSER = ArgumentParser()
+# Default libsemodule priority is 400
+_SEMODULE_ARG_PARSER.add_argument(
+    "--priority", "-X", action="store", type=int, default=400
+)
+_SEMODULE_ARG_PARSER.add_argument(
+    "--install", "-i", action="extend", nargs="*", default=[]
+)
+
+
+def list_semodule_installs(script: str) -> Iterable[tuple[str, int]]:
+    for cmd in get_command_executions(script, re.compile(r"(?:/usr/sbin/)?semodule")):
+        args, _ = _SEMODULE_ARG_PARSER.parse_known_args(cmd[1:])
+        for file in args.install:
+            # TODO: Possibly handle spaces better in parsing
+            yield file.strip(), args.priority
---
+++ whimse/utils/shell.py
@@ -0,0 +1,125 @@
+import re
+import shlex
+from collections.abc import Iterable
+from itertools import chain
+from pathlib import Path
+from re import Pattern
+
+
+def _expand_single(
+    tokens: tuple[str, ...], name: str, values: set[str]
+) -> Iterable[tuple[str, ...]]:
+    for value in values:
+        yield tuple(
+            token.replace(f"${{{name}}}", value).replace(f"${name}", value)
+            for token in tokens
+        )
+
+
+def _expand_env(
+    tokens: tuple[str, ...], env: dict[str, set[str]]
+) -> Iterable[tuple[str, ...]]:
+    old_partially_expanded: list[tuple[str, ...]] = [tokens]
+    for name, values in sorted(env.items(), key=lambda kv: len(kv[0]), reverse=True):
+        new_partially_expanded: list[tuple[str, ...]] = []
+        for partially_expanded in old_partially_expanded:
+            new_partially_expanded.extend(
+                _expand_single(partially_expanded, name, values)
+            )
+        old_partially_expanded = new_partially_expanded
+    yield from old_partially_expanded
+
+
+def _get_commands(tokens: list[str]) -> Iterable[tuple[str, ...]]:
+    cmd: list[str] = []
+    for token in tokens:
+        if token in ("||", "&&", "&", "|", ";", "do"):
+            if len(cmd) > 0:
+                yield tuple(cmd)
+                cmd = []
+        elif len(token) > 0 and token[-1] == ";":
+            cmd.append(token[:-1])
+            yield tuple(cmd)
+            cmd = []
+        else:
+            cmd.append(token)
+    if len(cmd) > 0:
+        yield tuple(cmd)
+
+
+def get_command_executions(
+    script: str, cmd_pattern: Pattern, env: dict[str, set[str]] | None = None
+) -> Iterable[tuple[str, ...]]:
+    lines = iter(script.splitlines())
+    if env is None:
+        env = {}
+    while True:
+        line = None
+        try:
+            line = next(lines)
+            while len(line) >= 1 and line[-1] == "\\":
+                line = line[:-1]
+                # Separate to remove '\' even if next() raises StopIteration here
+                line += " " + next(lines)
+        except StopIteration:
+            if line is None:
+                return
+
+        while True:
+            try:
+                tokens = shlex.split(line, comments=True)
+                break
+            except ValueError as ex:
+                if ex.args != ("No closing quotation",):
+                    raise
+                try:
+                    line += next(lines)
+                except StopIteration:
+                    raise ex from None
+
+        for cmd in _get_commands(tokens):
+            if (
+                len(cmd) == 2
+                and cmd[0] == "export"
+                and re.match(r"^(?P<name>[a-zA-Z0-9_]+)\+?=(?P<value>.*)$", cmd[1])
+            ):
+                cmd = cmd[1:]
+            if len(cmd) > 0 and cmd_pattern.match(cmd[0]):
+                yield from _expand_env(cmd, env)
+            elif (
+                len(cmd) == 1
+                and (
+                    match := re.match(
+                        r"^(?P<name>[a-zA-Z0-9_]+)=(?P<value>.*)$", cmd[0]
+                    )
+                )
+                is not None
+            ):
+                env[match.group("name")] = {
+                    t[0] for t in _expand_env((match.group("value"),), env)
+                }
+            elif (
+                len(cmd) == 1
+                and (
+                    match := re.match(
+                        r"^(?P<name>[a-zA-Z0-9_]+)\+=(?P<value>.*)$", cmd[0]
+                    )
+                )
+                is not None
+            ):
+                expanded = list(_expand_env((match.group("value"),), env))
+                env[match.group("name")] = {
+                    f"{t1}{t2[0]}"
+                    for t1 in env.get(match.group("name"), {""})
+                    for t2 in expanded
+                }
+            elif len(cmd) >= 4 and cmd[0] == "for" and cmd[2] == "in":
+                env[cmd[1]] = set(chain(*_expand_env(cmd[3:], env)))
+            elif (
+                len(cmd) == 2
+                and cmd[0] == "."
+                and (include_path := Path(cmd[1])).is_file()
+            ):
+                yield from get_command_executions(
+                    include_path.read_text(encoding="local"), cmd_pattern, env
+                )
