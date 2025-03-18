bc76ea86a903ff68a14fab1c52df96fcfee3b4f3
Juraj Marcin <juraj@jurajmarcin.com>
2025-03-18 01:48:15 +0100

    whime.utils.subprocess: Add logging
    Signed-off-by: Juraj Marcin <juraj@jurajmarcin.com>

--- whimse/utils/subprocess.py
+++ whimse/utils/subprocess.py
@@ -1,3 +1,4 @@
+import shlex
 import subprocess
 from functools import wraps
 
@@ -12,13 +13,17 @@ def run(
     **outer_kwargs,
 ):
     def _run(*args, stdout=None, stderr=None, **kwargs):
-        process = subprocess.run(
-            *args,
-            stdout=stdout if stdout else subprocess.PIPE,
-            stderr=stderr if stderr else subprocess.PIPE,
-            check=check,
-            **kwargs,
-        )
-        return process
+        logger.verbose("Executing: %r", shlex.join(map(str, args[0])) if isinstance(args[0], list) else args[0])
+        try:
+            return subprocess.run(
+                *args,
+                stdout=stdout if stdout else subprocess.PIPE,
+                stderr=stderr if stderr else subprocess.PIPE,
+                check=check,
+                **kwargs,
+            )
+        except subprocess.CalledProcessError as ex:
+            logger.verbose("Execution failed with code %d stdout=%r stderr=%r", ex.returncode, ex.stdout, ex.stderr)
+            raise
 
     return _run(*outer_args, **outer_kwargs)
